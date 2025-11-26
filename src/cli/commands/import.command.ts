c867e9f1891c9fca0eace0b6da1e3fc9a0ab36e8
Anatolii Egorov <silkroadbum@mail.ru>
2025-11-26 15:02:42 +0300

    register import command

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,3 +1,4 @@
+import { TSVFileReader } from "../../shared/libs/file-reader/index.js";
 import { ICommand } from "./command.interface.js";
 
 export class ImportCommand implements ICommand {
@@ -5,6 +6,17 @@ export class ImportCommand implements ICommand {
     return "--import";
   }
   public execute(...parameters: string[]): void {
-    // Чтение файла
+    const [filename] = parameters;
+    const fileReader = new TSVFileReader(filename.trim());
+
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (err) {
+      if (!(err instanceof Error)) throw err;
+
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(`Details: ${err.message}`);
+    }
   }
 }
--- src/shared/libs/file-reader/index.ts
+++ src/shared/libs/file-reader/index.ts
@@ -1,1 +1,2 @@
 export { IFileReader } from "./file-reader.interface.js";
+export { TSVFileReader } from "./tsv-file-reader.js";
